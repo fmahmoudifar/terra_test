5442575287803feab7a962b9ceed9b207fd0f561
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2026-02-02 15:12:17 +0000

    added dep

---
+++ dep/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-2"
+  profile = "terra_test"
+}
+
+resource "aws_instance" "db" {
+    ami = "ami-06e3c045d79fd65d9"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "DB Server"
+    }
+  
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-06e3c045d79fd65d9"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "Web Server"
+    }
+    depends_on = [ aws_instance.db ]
+  
+}
